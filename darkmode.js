cab351db5359ff16d393f05c1380998289611212
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2025-01-14 16:24:04 +0000

    Added resume

---
+++ darkmode.js
@@ -0,0 +1,17 @@
+// let darkmode = localStorage.getItem("darkmode");
+// const themeSwitch = document.getElementById("theme-switch");
+
+// const enableDarkmode = () => {
+//   document.body.classList.add("dark-mode");
+//   localStorage.setItem("darkMode", "active");
+// };
+// const disableDarkmode = () => {
+//   document.body.classList.remove("dark-mode");
+//   localStorage.setItem("darkMode", "inactive");
+// };
+
+// if (darkMode === "active") enableDarkmode();
+
+// themeSwitch.addEventListener("click", () => {
+//   darkMode !== "active" ? enableDarkmode() : disableDarkmode();
+// });
